ca156bc1fb514627b6fb20c40be697e9d8dbb3c1
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-07-24 07:04:59 +0100

    fix blank page issue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,12 +12,13 @@ const allReducers = combineReducers({
 
 const middleWare = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   allReducers,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default store;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
